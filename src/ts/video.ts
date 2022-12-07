b7abbe6574b0995c47e8f1c30adceb563d7ac844
GensIto <ito@ateuniverse.com>
2022-12-07 12:19:55 +0900

    動画を画面サイズに合わせてソースを変える関数追加

---
+++ src/ts/video.ts
@@ -0,0 +1,28 @@
+export const video = (videoElement: HTMLVideoElement, pcSource: string, spSource: string) => {
+  const videoBreakpoint = () => {
+    const w = window.innerWidth;
+    const mdW = 768;
+    const newSource = document.createElement('source');
+
+    newSource.src = w >= mdW ? pcSource : spSource;
+    videoElement.appendChild(newSource);
+  };
+
+  const videoPause = () => {
+    videoElement.pause();
+  };
+
+  const videoPlay = () => {
+    videoElement.play();
+  };
+
+  window.addEventListener('load', () => {
+    videoBreakpoint();
+    videoPause();
+    setInterval(() => {
+      if (document.querySelector('.is-loaded')) {
+        videoPlay();
+      }
+    }, 1000);
+  });
+};
